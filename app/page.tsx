ea517eb1acf71cc4c57de88c6b0d9b34bec2bfdf
Joel K George <joelk9895@gmail.com>
2025-10-10 21:57:46 +0530

    Completed the import process

--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,11 @@
-import ContentList from "@/app/components/general/contentList";
-
-interface ContentItem {
-  id: string;
-  contentType: string;
-  contentName: string;
-  bpm: string;
-  key: string;
-  soundGroup: string;
-  subGroup: string;
-  styles: string[];
-  moods: string[];
-  processing: string[];
-  soundDesign: string[];
-  createdAt: string;
-  fileId: string;
-  file: {
-    fileName: string;
-    fileUrl: string;
-    awsKey: string;
-  };
-}
-interface ConstructionKit {
-  id: string;
-  kitName: string;
-  description: string;
-  bpm: string;
-  key: string;
-  styles: string[];
-  moods: string[];
-  userId: string;
-  contents: ContentItem[];
-  createdAt: string;
-}
-
 export default async function Home() {
-  let content: ContentItem[] = [];
-  let constructionKits: ConstructionKit[] = [];
-
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/content`, {
-      cache: "no-store",
-    });
-
-    if (!res.ok) {
-      throw new Error(`Failed to fetch content: ${res.statusText}`);
-    }
-
-    const data = await res.json();
-
-    content = data.contents as ContentItem[];
-    constructionKits = data.constructionKit as ConstructionKit[];
-  } catch (error: Error | unknown) {
-    console.error("Error fetching content:", error);
-    content = [];
-  }
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ContentList
-        items={content}
-        constructionKits={constructionKits}
-        showKits={true}
-      />
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <h1 className="text-4xl font-bold mb-8">Construction Kits</h1>
+      </div>
+
+      <div className="w-full max-w-5xl mt-8">Landing page coming soon!</div>
     </main>
   );
 }
